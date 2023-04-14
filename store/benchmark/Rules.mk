940ab2c0b314df76ebecb9943ed5a860b173aaca
ShreyaRavi <sravi2@stanford.edu>
2023-04-14 15:36:39 -0700

    fix retwis client compilation.

--- store/benchmark/Rules.mk
+++ store/benchmark/Rules.mk
@@ -1,11 +1,11 @@
 d := $(dir $(lastword $(MAKEFILE_LIST)))
 
-SRCS += $(addprefix $(d), benchClient.cc, retwisClient.cc)
+SRCS += $(addprefix $(d), benchClient.cc retwisClient.cc)
 
 OBJS-all-clients := $(OBJS-tapir-client)
 
 $(d)benchClient: $(OBJS-all-clients) $(o)benchClient.o
 
-$(d)benchClient: $(OBJS-all-clients) $(o)retwisClient.o
+$(d)retwisClient: $(OBJS-all-clients) $(o)retwisClient.o
 
 BINS += $(d)benchClient $(d)retwisClient
