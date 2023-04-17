0444f764e8d43122b7a04e061dd396b7b67b1e0b
Shreya Ravi <sravi2@stanford.edu>
2023-04-16 18:41:33 -0700

    remove prints. print total number of completed transactions

--- store/benchmark/retwisClient.cc
+++ store/benchmark/retwisClient.cc
@@ -297,11 +297,13 @@ main(int argc, char **argv)
             retries = client->Stats()[0];
         }
 
-        fprintf(stderr, "%d %ld.%06ld %ld.%06ld %ld %d %d %d", ++nTransactions, t1.tv_sec,
-                t1.tv_usec, t2.tv_sec, t2.tv_usec, latency, status?1:0, ttype, retries);
-        fprintf(stderr, "\n");
+        // fprintf(stderr, "%d %ld.%06ld %ld.%06ld %ld %d %d %d", ++nTransactions, t1.tv_sec,
+        //         t1.tv_usec, t2.tv_sec, t2.tv_usec, latency, status?1:0, ttype, retries);
+        // fprintf(stderr, "\n");
+        nTransactions++;
 
         if (((t2.tv_sec-t0.tv_sec)*1000000 + (t2.tv_usec-t0.tv_usec)) > duration*1000000) {
+            printf("number of completed transactions: %d\n", nTransactions);
             break;
         } 
     }
